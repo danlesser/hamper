3dae8d1bc4650767c681f2d791a50bdeedefbeeb
Dan Lesser <daniel.lesser@bt.com>
2015-04-14 14:54:53 +0100

    Tweak title regex

--- app/models/convention.rb
+++ app/models/convention.rb
@@ -1,5 +1,5 @@
 class Convention < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true,
-                    format: { with: /\A[a-zA-Z0-9 ]+\z/, message: 'Alphanumeric characters (and spaces) only.' }
+                    format: { with: /\A[[:alpha:]0-9 ]+\z/, message: 'Alphanumeric characters (and spaces) only.' }
   validates :convention_email, presence: true, uniqueness: true, email_format: { message: 'Valid email addresses only.' }
 end
