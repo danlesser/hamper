e54dbae562bad24a80dbb13c414f3e3e71f05f41
Dan Lesser <daniel.lesser@bt.com>
2015-04-15 15:31:48 +0100

    Scope uniqueness of tracks and days to conventions

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
-  validates :name, presence: true,
+  validates :name, presence: true, uniqueness: { scope: :convention },
                    format: { with: /\A[[:alpha:]0-9 ]+\z/, message: 'Alphanumeric characters (and spaces) only.' }
-  validates :rank, presence: true
+  validates :rank, presence: true, uniqueness: { scope: :convention }
   belongs_to :convention
 end
--- factories/day_factory.rb
+++ factories/day_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :day do
-    date { (rand(2.days.ago..5.minute.ago)).change(usec: 0).to_s }
+    date { (rand(100.days.ago..5.minute.ago)).change(usec: 0).to_s }
     name { Faker::Internet.domain_word }
   end
 end
