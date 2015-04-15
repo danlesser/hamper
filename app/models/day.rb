51ac2fb0dabdd56bb763d557a8b046601c7c7ee6
Dan Lesser <daniel.lesser@bt.com>
2015-04-15 15:22:10 +0100

    Temporarily remove uniqueness contraints on Day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,5 @@
 class Day < ActiveRecord::Base
-  validates :date, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true
+  validates :date, presence: true
+  validates :name, presence: true
   belongs_to :convention
 end
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe Day do
   it { is_expected.to validate_presence_of :date }
-  it { is_expected.to validate_uniqueness_of :date }
   it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_uniqueness_of :name }
   it { is_expected.to belong_to :convention }
 end
