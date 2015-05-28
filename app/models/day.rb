2395870d28c2d0e6a53f61c8252dab90d80c95b7
Dan Lesser <daniel.lesser@bt.com>
2015-05-28 14:49:27 +0100

    Tweak limit on day name a little

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,6 +1,6 @@
 class Day < ActiveRecord::Base
   validates :date, presence: true, uniqueness: { scope: :convention }, date:
     { after_or_equal_to: proc { Time.zone.now.to_date } }
-  validates :name, presence: true, uniqueness: { scope: :convention }, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: { scope: :convention }, length: { maximum: 30 }
   belongs_to :convention
 end
--- factories/day_factory.rb
+++ factories/day_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :day do
     date { (rand(Time.zone.now..Time.zone.now + 1.month)).change(usec: 0) }
-    name { Faker::Internet.domain_word[1..20] }
+    name { Faker::Internet.domain_word[1..30] }
   end
 end
