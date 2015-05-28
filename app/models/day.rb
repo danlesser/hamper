2e4c2ce8cae944ccc4edd914559979a94c06a40c
Dan Lesser <daniel.lesser@bt.com>
2015-05-28 15:03:31 +0100

    More validations

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,6 +1,7 @@
 class Day < ActiveRecord::Base
   validates :date, presence: true, uniqueness: { scope: :convention }, date:
     { after_or_equal_to: proc { Time.zone.now.to_date } }
-  validates :name, presence: true, uniqueness: { scope: :convention }, length: { maximum: 30 }
+  validates :name, presence: true, uniqueness: { scope: :convention }, length: { maximum: 30 },
+                   format: { with: /\A[[:alpha:]0-9 ]+\z/, message: 'Alphanumeric characters (and spaces) only.' }
   belongs_to :convention
 end
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -5,6 +5,7 @@ describe Day do
   it { is_expected.to validate_uniqueness_of(:date).scoped_to(:convention_id) }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of(:name).scoped_to(:convention_id) }
+  it { is_expected.to allow_value('Art').for(:name).with_message('Alphanumeric characters (and spaces) only.') }
   it { is_expected.to validate_length_of(:name).is_at_most(30) }
   it { is_expected.to belong_to :convention }
 end
