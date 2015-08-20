2daded232150e6e181bce52d3e5fa8dcafc4635b
Dan Lesser <daniel.lesser@bt.com>
2015-08-20 14:15:05 +0100

    Improve validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  validates :name, uniqueness: true
-  validates :email, uniqueness: true, email_format: { message: 'Valid email addresses only.' },
+  validates :name, presence: true,
+                   format: { with: /\A[\w ]+\z/, message: 'Alphanumeric characters (and spaces) only.' },
+                   length: { maximum: 40 }
+  validates :email, presence: true, uniqueness: true, email_format: { message: 'Valid email addresses only.' },
                     length: { maximum: 40 }
 end
